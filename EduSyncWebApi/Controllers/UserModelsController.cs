be79c8d6b14b72b9b2d913aba4be5c1f7defc908
SomyaGoyal <115345500+goyal2@users.noreply.github.com>
2025-05-31 19:46:04 +0530

    Update UserModelsController.cs

--- EduSyncWebApi/Controllers/UserModelsController.cs
+++ EduSyncWebApi/Controllers/UserModelsController.cs
@@ -25,7 +25,7 @@ namespace EduSyncWebApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserModel>>> GetUserModels()
         {
-            Console.Writeline("inside usermodel");
+            Console.WriteLine("inside usermodel");
             return await _context.UserModels.ToListAsync();
         }
 
